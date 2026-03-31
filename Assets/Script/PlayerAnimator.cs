9f511e845ddda3a7267fbff7ff1b74d594af645b
AlexiaPlf <apolifonte73@gmail.com>
2026-04-01 00:33:22 +0200

    Ajout d'un personnage joueur animé

---
+++ Assets/Script/PlayerAnimator.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private Vector2 lastMoveInput;
+
+    // Noms des paramètres dans l'Animator Controller
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private static readonly int FacingRight = Animator.StringToHash("facingRight");
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            Debug.LogError("Animator manquant sur le Player!");
+    }
+
+    // Appelé depuis PlayerMovement.cs à chaque SetMoveInput
+    public void UpdateAnimation(Vector2 moveInput)
+    {
+        bool isMoving = moveInput.sqrMagnitude > 0.01f;
+        animator.SetBool(IsMoving, isMoving);
+
+        // Met à jour la direction seulement si le joueur bouge
+        if (moveInput.x > 0.01f)
+            animator.SetBool(FacingRight, true);
+        else if (moveInput.x < -0.01f)
+            animator.SetBool(FacingRight, false);
+        // Si x == 0 (mouvement vertical), on conserve la dernière direction
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))] //ajoute rigid body si j'oublie
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 0.5f;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private PlayerAnimator playerAnimator;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<PlayerAnimator>();
     }
 
     // Cette fonction est appelée depuis Player.cs
     public void SetMoveInput(Vector2 input)
     {
         moveInput = input;
+        playerAnimator?.UpdateAnimation(input);
     }
 
     void FixedUpdate()
